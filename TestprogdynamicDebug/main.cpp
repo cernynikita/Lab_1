67617c5a0d4b61d6cb7c67f38fc5f0fe1b00493c
QTBoy-web <oleg.fomin2006@gmail.com>
2026-06-10 20:22:07 +0400

    Update main.cpp

--- TestprogdynamicDebug/main.cpp
+++ TestprogdynamicDebug/main.cpp
@@ -6,5 +6,6 @@ int main()
 {
     cout << "Hello Chernov Nikita Alexandrovich!" << endl;
     cout << "Hello, Prosyanov Viktor Nikolaevich" << endl;
+    cout << "Hello, Fomin Oleg Victorovich!" << endl;
     return 0;
 }
